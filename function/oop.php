8ba60ea385bc5da12911df299837ccbe39708a63
Shahlal47 <shahlalhoss@gmail.com>
2015-01-24 16:15:19 +0600

    clss work updated

--- function/oop.php
+++ function/oop.php
@@ -1,4 +1,27 @@
 
+<?php
 
-/* alert('I am ready');
-*   $('h1').addClass("test");*/
+    class User{
+
+       public $name=null;
+
+        function  __construct($name){
+            $this->name=$name;
+        }
+    }
+
+$arif=new User('Arif');
+$arifa=new User('Arifa');
+
+var_dump($arif);
+
+
+//    $arif=new User();
+//    $arif->name='Arif';
+//    echo $arif->name;
+//    var_dump($arif);
+//
+//    $arifa=new User();
+//    var_dump($arifa);
+
+
